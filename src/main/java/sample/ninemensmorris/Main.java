2175474f241a00a5fa101312f28c1874b729636f
Kisara Batugedara <kbat0008@student.monash.edu>
2023-04-27 21:20:03 +1000

    Added private attributes to Main class and added documentation for it

--- src/main/java/sample/ninemensmorris/Main.java
+++ src/main/java/sample/ninemensmorris/Main.java
@@ -7,12 +7,34 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
+/** Main Driver Class
+ */
 public class Main extends Application {
+
+
+    /**
+     * Game Instance
+     */
+    private Game game;
+
+    /**
+     * Tutorial Game Instance
+     */
+
+    private TutorialGame tutorialGame;
+
+
+    /**
+     * Boolean to check if app is still running
+     */
+    private Boolean appRunning;
+
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1000, 1000);
-        stage.setTitle("Hello!");
+        stage.setTitle("Welcome to Nine Men's Morris!");
         stage.setScene(scene);
         stage.show();
     }
