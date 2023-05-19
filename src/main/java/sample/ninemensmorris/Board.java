778b7543ced58bf140e6e42586f839526725d8a0
Sok Huot Ear <sear0002@student.monash.edu>
2023-05-19 14:42:51 +1000

    Mill detection works

--- src/main/java/sample/ninemensmorris/Board.java
+++ src/main/java/sample/ninemensmorris/Board.java
@@ -1,10 +1,7 @@
 package sample.ninemensmorris;
 import java.util.List;
 
-import javafx.geometry.Pos;
-
 import java.util.ArrayList;
-import java.util.Objects;
 
 public class Board {
 
@@ -48,10 +45,10 @@ public class Board {
                 {positions[21], null, null, positions[22], null, null, positions[23]},
         };
 
-        configureNeighbour();
+        configureNeighbourForPosition();
     }
 
-    private void configureNeighbour()
+    private void configureNeighbourForPosition()
     {
         int up_index = 0;
         int down_index = 1;
@@ -61,7 +58,6 @@ public class Board {
         {
             Position neighbours[] = new Position[4];
             int[] colRow = getColsAndRowsOf(position);
-
 
             // traverse up
             try {
@@ -153,90 +149,7 @@ public class Board {
     public boolean validateMove(Token token, Position destination) {
         return true;
     }
-//
-//    public boolean detectMill(Token placedToken) {
-//        // loop through all the tokens
-//        double x_intercept = placedToken.getShape().getLayoutX();
-//        double y_intercept = placedToken.getShape().getLayoutY();
-//
-//        // get token colour
-//        TokenColour colour = placedToken.getColour();
-//        ArrayList<Token> tokenListX = new ArrayList<>();
-//        ArrayList<Token> tokenListY = new ArrayList<>();
-//
-//        if (colour == TokenColour.BLACK)
-//        {
-//            tokenListX = getBlackTokensAtX(x_intercept);
-//            tokenListY = getBlackTokensAtY(y_intercept);
-//        }
-//
-//        if (colour == TokenColour.WHITE)
-//        {
-//            tokenListX = getWhiteTokensAtX(x_intercept);
-//            tokenListY = getWhiteTokensAtY(y_intercept);
-//        }
-//
-//        if (tokenListX.size() <= 3 || tokenListY.size() <= 3)
-//        {
-//            return true;
-//        }
-//        else
-//        {
-//            return false;
-//        }
-//    }
-//
-//    public ArrayList<Token> getWhiteTokensAtX(double x_intercept) {
-//        ArrayList<Token> tokenListX = new ArrayList<>();
-//
-//        // loop through all the tokens
-//        for (Token token : whiteTokens) {
-//            // Code to be executed for each token
-//            if (token.getPosition().getShape().getLayoutX() == x_intercept) {
-//                tokenListX.add(token);
-//            }
-//        }
-//        return tokenListX;
-//    }
-//
-//    public ArrayList<Token> getWhiteTokensAtY(double y_intercept) {
-//        ArrayList<Token> tokenListY = new ArrayList<>();
-//
-//        // loop through all the tokens
-//        for (Token token : whiteTokens) {
-//            // Code to be executed for each token
-//            if (token.getPosition().getShape().getLayoutY() == y_intercept) {
-//                tokenListY.add(token);
-//            }
-//        }
-//        return tokenListY;
-//    }
-//
-//    public ArrayList<Token> getBlackTokensAtX(double x_intercept) {
-//        ArrayList<Token> tokenListX = new ArrayList<>();
-//
-//        // loop through all the tokens
-//        for (Token token : blackTokens) {
-//            // Code to be executed for each token
-//            if (token.getPosition().getShape().getLayoutX() == x_intercept) {
-//                tokenListX.add(token);
-//            }
-//        }
-//        return tokenListX;
-//    }
-//
-//    public ArrayList<Token> getBlackTokensAtY(double y_intercept) {
-//        ArrayList<Token> tokenListY = new ArrayList<>();
-//
-//        // loop through all the tokens
-//        for (Token token : blackTokens) {
-//            // Code to be executed for each token
-//            if (token.getPosition().getShape().getLayoutY() == y_intercept) {
-//                tokenListY.add(token);
-//            }
-//        }
-//        return tokenListY;
-//    }
+
 
 
     public boolean isWhiteTokenAt(Position targetPosition){
